8b2f61318c5b1637496581f84cfecfa07e57c0d7
Satyum <satyumspathania@gmail.com>
2023-09-11 00:31:56 +0530

    Completed First screen and moving on to questions activity.

--- app/src/main/java/com/example/quiz_app/QuizQuestions.kt
+++ app/src/main/java/com/example/quiz_app/QuizQuestions.kt
@@ -3,13 +3,9 @@ package com.example.quiz_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class QuizQuestions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-//        val btn_start : Button = findViewById(R.id.btn_start)
-//        val et_name : btn
-
+        setContentView(R.layout.activity_quiz_questions)
     }
 }
