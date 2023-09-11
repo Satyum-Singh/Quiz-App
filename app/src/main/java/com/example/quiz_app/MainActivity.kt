98345a57245e5d1176ae537ff124288499d91373
Satyum <satyumspathania@gmail.com>
2023-09-11 09:14:59 +0530

    rechecking things.

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -2,14 +2,23 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val btn_start : Button = findViewById(R.id.btn_start)
-//        val et_name : btn
+        val btn_start : Button = findViewById(R.id.btn_start)
+        val et_name : EditText = findViewById(R.id.et_name)
+
+        btn_start.setOnClickListener{
+            if(et_name.text.isEmpty()){
+                Toast.makeText(this,"Please enter your name to begin",Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/quiz_app/QuizQuestions.kt
+++ app/src/main/java/com/example/quiz_app/QuizQuestions.kt
@@ -2,10 +2,13 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class QuizQuestions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
     }
 }
